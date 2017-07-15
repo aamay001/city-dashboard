c6c2a8ca2579c8428ed26f0fe9078415d3ee16f4
Andy Amaya <aamay001@gmail.com>
2017-07-14 20:21:18 -0700

    restore call to unsplash api

--- js/app.js
+++ js/app.js
@@ -1,7 +1,5 @@
 'use strict';
 
-var backgroundUrl = 'https://images.unsplash.com/photo-1417163434503-8b20d5adfe36?ixlib=rb-0.3.5&q=85&fm=jpg&crop=entropy&cs=srgb&s=cc4b332e282f44b9a0995ea2fa33cf7f'
-
 const CD_GOOGLE_APIS = {
     GEOCODING : { 
         url : 'https://maps.googleapis.com/maps/api/geocode/json',
@@ -94,9 +92,8 @@ function setMapBackground() {
     let params = {
         client_id : CD_UNSPLASH_API.client_id,
         query : 'scenery'
-    }
+    }    
     
-    /*
     $.getJSON(CD_UNSPLASH_API.url, params)
         .then(function(res){
             if ( res.urls) {
@@ -106,12 +103,8 @@ function setMapBackground() {
             }
         })
         .fail(function(err) {
-
-        });
-        */
-
-    $('body').css('background-image', 'url(' + backgroundUrl + ')');
-    $(CD_HTML.mapBackground).fadeIn('slow');
+            console.log(err);
+        });        
 }
 
 function showWeatherInformation(res) { 
