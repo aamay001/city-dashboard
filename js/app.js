46455c8d5a1eee56f8ff7a0c8971867aa4c49985
Andy Amaya <aamay001@gmail.com>
2017-07-14 19:43:19 -0700

    refactored geolocation feature

--- js/app.js
+++ js/app.js
@@ -55,11 +55,7 @@ function onReady() {
 
     cdAutcomplete.addListener('place_changed', onPlaceChanged);
 
-    let locationPromise = new Promise( (resolve, reject ) => {
-        getUserLocation(resolve, reject);
-    });
-
-    locationPromise.then(showDashboard).catch(showSearch); 
+    getUserLocation();
 }
 
 function onPlaceChanged() {
@@ -148,7 +144,7 @@ function showWeatherError(err) {
 ///////////////////////////////////////////////////////////////////////////////
 // Geo Location
 ///////////////////////////////////////////////////////////////////////////////
-function getUserLocation(resolve, reject) {
+function getUserLocation() {
     let geolocationOps = {
             enableHighAccuracy: true, 
             maximumAge        : 30000, 
@@ -156,63 +152,47 @@ function getUserLocation(resolve, reject) {
         };
 
     if (navigator.geolocation) {
-        navigator.geolocation.getCurrentPosition(
-            function(position){ getPositionSuccess(position, resolve, reject) }, 
-            function(err) { getPositionError(err, reject) }, geolocationOps);        
+        navigator.geolocation.getCurrentPosition( getUserLocality, handleError, geolocationOps);        
+    } else {
+        // use form to have user enter location
+        showSearch();
     }
-
-    else
-        reject('Geolocation unavailable.');
 }
 
-function getPositionSuccess(position, resolve, reject) {
+function getUserLocality(position) {
     cdUserLocation.longitude = position.coords.longitude;
     cdUserLocation.latitude = position.coords.latitude;    
-    console.log( `User location updated: ${cdUserLocation.longitude}, ${cdUserLocation.latitude}` );
-    getUserLocality(resolve, reject);
-}
 
-function getPositionError(err, reject) {
-    console.log("Error getting user's location.");
-    console.log(err.message);
-    reject("Unable to get user's location.");
-}
-
-function getUserLocality(resolve, reject) {
     let params = {
                     latlng : cdUserLocation.latitude + ',' + cdUserLocation.longitude,
                     key : CD_GOOGLE_APIS.GEOCODING.key
                  }
     $.getJSON(CD_GOOGLE_APIS.GEOCODING.url, params)
-        .then( function(res) { getLocalitySuccess(res, resolve, reject) })
-        .fail( function(err) { getLocalityError(err, reject) }); 
+        .then(function(res){
+            console.log(res);
+            if ( res.results)    {
+                let cities = res.results.map(
+                    function(item) {
+                        if ( item.formatted_address && "locality" === item.types[0] )
+                            return item.formatted_address;
+                    }).filter(
+                    function(item) { 
+                        if ( item ) return item;
+                    });
+                
+                cdUserLocation.city = cities;    
+                console.log( `User location updated: ${cdUserLocation.city}` );
+            } else {
+                console.log('User locality could not be determined.');
+            }
+        });
 }
 
-function getLocalitySuccess(res, resolve, reject) {
-    if ( res.results)
-    {
-        let cities = res.results.map(
-            function(item) {
-                if ( item.formatted_address && "locality" === item.types[0] )
-                    return item.formatted_address;
-            }).filter(
-            function(item) { 
-                if ( item ) return item;
-            });
-        
-        cdUserLocation.city = cities;    
-        console.log( `User location updated: ${cdUserLocation.city}` );
-        resolve(cdUserLocation);
-    } else {
-        console.log('User locality could not be determined.');
-        reject('The results returned by Google were not determinate.');
-    }
-}
+function handleError(err){
+    console.log(err);
 
-function getLocalityError(err, reject) {
-    console.log('Error talking to Google Geocoding API.');
-    console.log(err.message);
-    reject("Unable to get user's locality.");
+    if ( err.code === 1)
+        showSearch();
 }
 
 ///////////////////////////////////////////////////////////////////////////////
