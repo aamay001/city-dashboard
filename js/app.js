75d55006597b968a904ef56e5ad41650d93e619b
Andy Amaya <aamay001@gmail.com>
2017-07-16 08:10:37 -0700

    card elements styling

--- js/app.js
+++ js/app.js
@@ -277,7 +277,7 @@ function getWeatherDataHTML() {
                     <div class="card blue lighten-4 grey-text text-darken-4">
                         <div class="card-content">
                         <img src="https://developer.accuweather.com/sites/default/files/${item.Day.Icon}-s.png" alt='Icon for current weather.' style="float:right"/>
-                        <span class="card-title">${item.Temperature.Maximum.Value}°F</span>
+                        <span class="card-title">${item.Temperature.Maximum.Value}°F</span><br>
                         <i>${item.Day.IconPhrase}</i><br>
                         <sub><i class="fa fa-arrow-down" aria-hidden="true"></i> low: ${item.Temperature.Minimum.Value}°F <i class="fa fa-arrow-up" aria-hidden="true"></i> high: ${item.Temperature.Maximum.Value}°F</sub>
                         </div>
